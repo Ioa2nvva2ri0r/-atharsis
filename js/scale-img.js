1a2b8513ee5e92a3af6c14d8cc38e09aefce72ed
Johnvva2ri0r <paf-gr8.johnvva2ri0r.yandex.by>
2023-05-10 12:29:51 +0300

    Updates

--- js/scale-img.js
+++ js/scale-img.js
@@ -2,8 +2,10 @@ const image = document.querySelectorAll('.common-form__img');
 
 const observerImage = new IntersectionObserver(
   ([entry]) => {
-    if (entry.isIntersecting) image.forEach((el) => el.classList.add('active'));
-    else image.forEach((el) => el.classList.remove('active'));
+    if (window.screen.width > 768)
+      if (entry.isIntersecting)
+        image.forEach((el) => el.classList.add('active'));
+      else image.forEach((el) => el.classList.remove('active'));
   },
   {
     threshold: [0.4],
@@ -11,4 +13,4 @@ const observerImage = new IntersectionObserver(
 );
 observerImage.observe(...image);
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY2FsZS1pbWcuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgaW1hZ2UgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcuY29tbW9uLWZvcm1fX2ltZycpO1xyXG5cclxuY29uc3Qgb2JzZXJ2ZXJJbWFnZSA9IG5ldyBJbnRlcnNlY3Rpb25PYnNlcnZlcihcclxuICAoW2VudHJ5XSkgPT4ge1xyXG4gICAgaWYgKGVudHJ5LmlzSW50ZXJzZWN0aW5nKSBpbWFnZS5mb3JFYWNoKChlbCkgPT4gZWwuY2xhc3NMaXN0LmFkZCgnYWN0aXZlJykpO1xyXG4gICAgZWxzZSBpbWFnZS5mb3JFYWNoKChlbCkgPT4gZWwuY2xhc3NMaXN0LnJlbW92ZSgnYWN0aXZlJykpO1xyXG4gIH0sXHJcbiAge1xyXG4gICAgdGhyZXNob2xkOiBbMC40XSxcclxuICB9XHJcbik7XHJcbm9ic2VydmVySW1hZ2Uub2JzZXJ2ZSguLi5pbWFnZSk7XHJcbiJdLCJmaWxlIjoic2NhbGUtaW1nLmpzIn0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY2FsZS1pbWcuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgaW1hZ2UgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcuY29tbW9uLWZvcm1fX2ltZycpO1xyXG5cclxuY29uc3Qgb2JzZXJ2ZXJJbWFnZSA9IG5ldyBJbnRlcnNlY3Rpb25PYnNlcnZlcihcclxuICAoW2VudHJ5XSkgPT4ge1xyXG4gICAgaWYgKHdpbmRvdy5zY3JlZW4ud2lkdGggPiA3NjgpXHJcbiAgICAgIGlmIChlbnRyeS5pc0ludGVyc2VjdGluZylcclxuICAgICAgICBpbWFnZS5mb3JFYWNoKChlbCkgPT4gZWwuY2xhc3NMaXN0LmFkZCgnYWN0aXZlJykpO1xyXG4gICAgICBlbHNlIGltYWdlLmZvckVhY2goKGVsKSA9PiBlbC5jbGFzc0xpc3QucmVtb3ZlKCdhY3RpdmUnKSk7XHJcbiAgfSxcclxuICB7XHJcbiAgICB0aHJlc2hvbGQ6IFswLjRdLFxyXG4gIH1cclxuKTtcclxub2JzZXJ2ZXJJbWFnZS5vYnNlcnZlKC4uLmltYWdlKTtcclxuIl0sImZpbGUiOiJzY2FsZS1pbWcuanMifQ==
